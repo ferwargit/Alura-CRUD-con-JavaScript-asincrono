c3add5dc39985bffc4cacb8aed395c23199e54b5
Fernando Warno <ferwar@hotmail.com>
2022-12-19 19:15:49 -0300

    Reescribiendo el Fetch API

--- AULA02/service/client-service.js
+++ AULA02/service/client-service.js
@@ -38,24 +38,41 @@ const table = document.querySelector("[data-table]");
 // Update - PUT/PATCH
 // Delete - DELETE
 
-const listaClientes = () => {
-  const promise = new Promise((resolve, reject) => {
-    const http = new XMLHttpRequest();
-    http.open("GET", "http://localhost:3000/perfiles");
 
-    http.send();
+// const listaClientes = () => {
+//   const promise = new Promise((resolve, reject) => {
+//     const http = new XMLHttpRequest();
+//     http.open("GET", "http://localhost:3000/perfiles");
 
-    http.onload = () => {
-      const response = JSON.parse(http.response);
-      if (http.status >= 400) {
-        reject(response);
-      } else {
-        resolve(response);
-      }
-    };
-  });
-  return promise;
-};
+//     http.send();
+
+//     http.onload = () => {
+//       const response = JSON.parse(http.response);
+//       if (http.status >= 400) {
+//         reject(response);
+//       } else {
+//         resolve(response);
+//       }
+//     };
+//   });
+//   return promise;
+// };
+
+// Fetch API
+// const listaClientes = () => {
+//   return fetch("http://localhost:3000/perfil")
+//     .then((respuesta) => {
+//       return respuesta.json();
+//     });
+// };
+
+// Otra forma de hacerlo
+// const listaClientes = () => {
+//   return fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
+// };
+
+// Otra forma de hacerlo
+const listaClientes = () => fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
 
 listaClientes()
   .then((data) => {
