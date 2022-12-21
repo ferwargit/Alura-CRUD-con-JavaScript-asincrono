290d03acd29c0ad790fd91efd2e0e44c6349ff23
Fernando Warno <ferwar@hotmail.com>
2022-12-20 22:57:36 -0300

    registro de cliente

---
+++ AULA03/controllers/registro.controller.js
@@ -0,0 +1,30 @@
+// Tengo que importar el modulo para poder usar la funcion
+import { clientServices } from "../service/client-service.js";
+
+
+const formulario = document.querySelector("[data-form]");
+
+// Recibe 2 parametros: 
+// 1. El evento que se va a escuchar 
+// 2. La funcion que se va a ejecutar cuando se dispare el evento
+// Nosotroa aqui recibimos el evento
+// Dentro del evento tenemos una propiedad que se llama preventDefault
+formulario.addEventListener('submit', (evento) => {
+  // console.log('formulario')
+  // No queremos que este formulario funcione como lo hace por defecto
+  evento.preventDefault();
+  // Tenemos que obtener los datos del formulario
+  const nombre = document.querySelector("[data-nombre]").value;
+  const email = document.querySelector("[data-email]").value;
+  console.log(nombre, email);
+  // Ahora que ya tenemos la informacion del formulario, tenemos que enviarla al servidor y eso se lo delegamos a un servicio o funcion querreciba esos datos y los envie al servidor.
+  clientServices
+    .crearCliente(nombre, email)
+    .then((respuesta) => {
+      // console.log(respuesta);
+      // Aqui le decimos cual es el archivo que queremos que se abra
+      window.location.href = "../screens/registro_completado.html";
+  }).catch((error) => {
+    console.log(error);
+  });
+});
--- AULA03/service/client-service.js
+++ AULA03/service/client-service.js
@@ -1,6 +1,26 @@
 const listaClientes = () =>
   fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
 
+const crearCliente = (nombre, email) => {
+  // Tenemos que definir nuestro fetch
+  // Le decimos la url a la que se va a conectar y despues
+  // va a recibir como 2do parametro un objeto con las opciones
+  // Aqui definimos las demas propiedades de la peticion
+  return fetch("http://localhost:3000/perfil", {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      nombre,
+      email,
+      // Generamos el id con la libreia uuid
+      id: uuid.v4()
+    })
+  });
+}
+
 export const clientServices = {
   listaClientes,
+  crearCliente,
 };
